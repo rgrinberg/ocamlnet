286638e62c5c81dc33d8bbd9fb5026a57e814083
gerd <gerd@96e5b9e0-aa12-0410-9e2e-b23d24e56262>
2006-05-09 23:20:51 +0000

    Continued.
    git-svn-id: https://godirepo.camlcity.org/svn/lib-ocamlnet2/trunk/code@884 96e5b9e0-aa12-0410-9e2e-b23d24e56262

--- src/netplex/netplex_vexec.mli
+++ src/netplex/netplex_vexec.mli
@@ -2,11 +2,11 @@
 
 (** Virtual Execution
   *
-  * The "vexec" protocol allows to invoke a routine over a socket like
+  * The VEXEC protocol allows to invoke a routine over a socket like
   * a Unix command. One can pass an environment, a command name, and
   * command arguments. One is connected with stdin, stdout and stderr.
   *
-  * "vexec" is only available over Unix domain sockets.
+  * VEXEC is only available over Unix domain sockets.
  *)
 
 open Netplex_types
@@ -23,6 +23,7 @@ object
     stderr:Unix.file_descr ->
     env:string array -> 
     cmd:string array ->
+    container ->
       int
 	(** Execute the command and return the exit code. The descriptors
           * must be closed by the command.
@@ -31,6 +32,18 @@ end
 
 
 val create_vexec_service : vexec_provider -> socket_service
+
+val vexec_process : (string array -> int) -> vexec_provider
+  (** Turns the current process into a VEXEC provider:
+    * - The virtual channels are connected with the real stdin, stdout, stderr
+    *   descriptors
+    * - The virtual environment is written into the process environment
+    * - The function is called with the command. The returned int is
+    *   interpreted as exit code
+    *
+    * Works only in multi-processing mode. The container is shut down
+    * after the VEXEC call is finished.
+   *)
 
 val connect_vexec_service :
        when_connected:(stdin:Unix.file_descr ->
