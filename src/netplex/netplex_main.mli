8fcd670184b9225c9a29db007e006ed1fd1ec076
gerd <gerd@96e5b9e0-aa12-0410-9e2e-b23d24e56262>
2006-07-25 13:23:44 +0000

    Docs.
    git-svn-id: https://godirepo.camlcity.org/svn/lib-ocamlnet2/trunk/code@955 96e5b9e0-aa12-0410-9e2e-b23d24e56262

--- src/netplex/netplex_main.mli
+++ src/netplex/netplex_main.mli
@@ -40,3 +40,40 @@ val startup :
     * You can override it by whatever parser you would like to use.
    *)
 
+
+(** {2 Tutorial}
+  *
+  * The typical main program for a [Netplex] server system looks like:
+  *
+  * {[ 
+  *   let my_factories = ...
+  *
+  *   let start() =
+  *    let opts, cmdconf = Netplex_main.args() in
+  *    Arg.parse 
+  *      opts
+  *      (fun s -> raise(Arg.Bad ("Unknown arg: " ^ s))) 
+  *      "usage: protoserver";
+  *    let par = Netplex_mp.mp() in  (* or Netplex_mt.mt() *)
+  *    Netplex_main.startup
+  *      par
+  *      Netplex_log.logger_factories
+  *      Netplex_workload.workload_manager_factories
+  *      my_factories
+  *      cmdconf
+  *
+  *   Sys.set_signal Sys.sigpipe Sys.Signal_ignore;
+  *   start()
+  * ]}
+  *
+  * This main program enables:
+  * - The standard command-line arguments [-conf], [-pid] and [-fg] are 
+  *   understood
+  * - The configuration file is parsed
+  * - The configuration can refer to all loggers and workload managers
+  *   coming with Netplex
+  * - The parallelizer is selected: Here, it is multi-processing
+  *   (Netplex_mp). One could also select multi-threading (Netplex_mt)
+  * - The processors defined by [my_factories] are made available
+  *   for connection processing
+ *)
