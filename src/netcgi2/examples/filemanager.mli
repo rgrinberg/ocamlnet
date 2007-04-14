7e0e43e8dd45e5ba5053535897ffdd290d761a55
ChriS <ChriS@96e5b9e0-aa12-0410-9e2e-b23d24e56262>
2007-04-14 13:37:51 +0000

    * code/src/netcgi2/examples moved to code/examples/cgi/netcgi2.
    git-svn-id: https://godirepo.camlcity.org/svn/lib-ocamlnet2/trunk/code@1115 96e5b9e0-aa12-0410-9e2e-b23d24e56262

--- src/netcgi2/examples/filemanager.mli
+++
@@ -1,8 +0,0 @@
-(* $Id: filemanager.mli,v 1.1 2005/09/04 11:45:21 chris_77 Exp $ *)
-
-open Netcgi
-
-val script :
-  (config:config -> arg_store:arg_store -> output_type:output_type ->
-    (#Netcgi.cgi -> unit) -> unit)
-  -> unit
