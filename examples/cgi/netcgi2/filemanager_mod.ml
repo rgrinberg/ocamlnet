3874aa57dab1b8e60b11152830bf19548348ff72
ChriS <ChriS@96e5b9e0-aa12-0410-9e2e-b23d24e56262>
2007-05-20 16:05:20 +0000

    Netcgi_apache replace Netcgi_mod.
    git-svn-id: https://godirepo.camlcity.org/svn/lib-ocamlnet2/trunk/code@1124 96e5b9e0-aa12-0410-9e2e-b23d24e56262

--- examples/cgi/netcgi2/filemanager_mod.ml
+++ examples/cgi/netcgi2/filemanager_mod.ml
@@ -1,13 +1,11 @@
-(* $Id: filemanager_mod.ml,v 1.1 2005/09/04 11:45:22 chris_77 Exp $ *)
+(** This program must be compiled to .cmo or .cma.  Moreover, put in
+    an Apache conf file:
 
-(** This program must be compiled to .cmo.  Moreover, put in a Apache
-    conf file:
-
-    CamlLoad nums.cma
-    CamlLoad numerix/numerix.cma
-    CamlLoad cryptokit/cryptokit.cma
+    NetcgiLoad nums.cma
+    NetcgiLoad numerix/numerix.cma
+    NetcgiLoad cryptokit/cryptokit.cma
 *)
 
 let () =
   Filemanager.script (fun ~config ~arg_store ~output_type f ->
-			Netcgi_mod.run ~config ~arg_store ~output_type f)
+			Netcgi_apache.run ~config ~arg_store ~output_type f)
