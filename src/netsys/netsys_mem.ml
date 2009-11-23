3eda32ee3e071493efaa08ea785e161c2b46b22f
gerd <gerd@96e5b9e0-aa12-0410-9e2e-b23d24e56262>
2009-11-23 02:23:47 +0000

    added
    git-svn-id: https://godirepo.camlcity.org/svn/lib-ocamlnet2/trunk/code@1299 96e5b9e0-aa12-0410-9e2e-b23d24e56262

---
+++ src/netsys/netsys_mem.ml
@@ -0,0 +1,83 @@
+(* $Id$ *)
+
+type memory = 
+    (char,Bigarray.int8_unsigned_elt,Bigarray.c_layout) Bigarray.Array1.t
+
+external netsys_blit_memory_to_string :
+           memory -> int -> string -> int -> int -> unit
+  = "netsys_blit_memory_to_string"
+
+external netsys_blit_string_to_memory : 
+           string -> int -> memory ->  int -> int -> unit
+  = "netsys_blit_string_to_memory"
+
+let blit_memory_to_string mem memoff s soff len =
+  let memlen = Bigarray.Array1.dim mem in
+  let slen = String.length s in
+  if len < 0 || memoff < 0 || memoff > memlen - len || 
+     soff < 0 || soff > slen - len 
+  then
+    invalid_arg "Netsys_mem.blit_memory_to_string";
+  netsys_blit_memory_to_string mem memoff s soff len
+
+let blit_string_to_memory s soff mem memoff len =
+  let memlen = Bigarray.Array1.dim mem in
+  let slen = String.length s in
+  if len < 0 || memoff < 0 || memoff > memlen - len || 
+     soff < 0 || soff > slen - len 
+  then
+    invalid_arg "Netsys_mem.blit_string_to_memory";
+  netsys_blit_string_to_memory s soff mem memoff len
+
+external memory_address : memory -> nativeint
+  = "netsys_memory_address"
+
+external getpagesize : unit -> int
+  = "netsys_getpagesize"
+
+external netsys_alloc_memory_pages : nativeint -> int -> memory
+  = "netsys_alloc_memory_pages"
+
+let alloc_memory_pages ?(addr=0n) len =
+  netsys_alloc_memory_pages addr len
+
+external alloc_aligned_memory : int -> int -> memory
+  = "netsys_alloc_aligned_memory"
+
+external memory_unmap_file : memory -> unit
+  = "netsys_memory_unmap_file"
+
+external as_value : memory -> int -> 'a
+  = "netsys_as_value"
+
+external netsys_init_string : memory -> int -> int -> unit
+  = "netsys_init_string"
+
+let init_string_bytelen len =
+  let ws = Sys.word_size / 8 in  (* word size in bytes *)
+  ((len + ws) / ws + 1) * ws
+  
+
+exception Out_of_space
+
+let init_string mem offset len =
+  let ws = Sys.word_size / 8 in  (* word size in bytes *)
+  let memlen = Bigarray.Array1.dim mem in
+  if offset < 0 || len < 0 then
+    invalid_arg "Netsys_mem.init_string";
+  let blen = init_string_bytelen len in
+  if blen > memlen - offset then
+    raise Out_of_space;
+  netsys_init_string mem offset len;
+  (ws, blen)
+
+
+external netsys_mem_read : Unix.file_descr -> memory -> int -> int -> int
+  = "netsys_mem_read"
+
+let mem_read fd mem off len =
+  if len < 0 || off < 0 || len > Bigarray.Array1.dim mem - off then
+    invalid_arg "Netsys_mem.mem_read";
+  netsys_mem_read fd mem off len
+
+    
---
+++ src/netsys/netsys_mem.mli
@@ -0,0 +1,201 @@
+(* $Id$ *)
+
+(** Bigarrays as memory buffers *)
+
+type memory = 
+    (char,Bigarray.int8_unsigned_elt,Bigarray.c_layout) Bigarray.Array1.t
+  (** We consider 1-dimensional bigarrays of chars as memory buffers.
+      They have the useful property that the garbage collector cannot
+      relocate them, i.e. the address is fixed. Also, one can mmap
+      a file, and connect the bigarray with shared memory.
+   *)
+
+(** {2 General} *)
+
+val blit_memory_to_string : memory -> int -> string -> int -> int -> unit
+  (** [blit_memory_to_string src srcoff dst dstoff len] copies [len] characters
+      from buffer [src], starting at character number [srcoff], to
+      string [dst], starting at character number [dstoff]
+
+      Raise [Invalid_argument] if [srcoff] and [len] do not
+      designate a valid subbuffer of [src], or if [dstoff] and [len]
+      do not designate a valid substring of [dst]. *)
+
+val blit_string_to_memory : string -> int -> memory ->  int -> int -> unit
+  (** [blit_string_to_memory src srcoff dst dstoff len] copies [len] characters
+      from string [src], starting at character number [srcoff], to
+      buffer [dst], starting at character number [dstoff]
+
+      Raise [Invalid_argument] if [srcoff] and [len] do not
+      designate a valid substring of [src], or if [dstoff] and [len]
+      do not designate a valid subbuffer of [dst]. *)
+
+val memory_address : memory -> nativeint
+  (** Returns the start address of the buffer *)
+
+(** {2 Allocation and memory-mapping} *)
+
+val getpagesize : unit -> int
+  (** Returns the size of a page.
+
+      On many systems, a page has 4096 bytes, but this cannot be relied
+      upon.
+
+      This function is only available if the system has [sysconf].
+   *)
+
+val alloc_memory_pages : ?addr:nativeint -> int -> memory
+  (** Allocates memory in units of pages. The memory buffer will start
+      on a page boundary.
+
+      The passed int is the requested number of {b bytes}. The size of
+      the buffer is rounded up so a whole number of pages is allocated.
+
+      Optionally, one can request a certain address [addr] (which must
+      be a multiple of the page size). There is, however, no guarantee
+      that this wish can be fulfilled. In any way, one should check with
+      [memory_address] what the start address really is.
+
+      This function is only available if the system has [sysconf], [mmap],
+      and allows to allocate anonymous memory with [mmap] (outside POSIX
+      but common).
+   *)
+
+val alloc_aligned_memory : int -> int -> memory
+  (** [alloc_aligned_memory alignment size]: Allocates a buffer of [size]
+      whose start address is a multiple of [alignment]. The [alignment]
+      must be a power of two, and at least [Sys.word_size/8].
+
+      Aligned memory can be useful for ensuring that the whole memory
+      block is in the same cache line. A cache line typically has
+      128 bytes - but this is very platform-specific.
+
+      This function is only available if the system has [posix_memalign].
+   *)
+
+(*
+val memory_map_file : Unix.file_descr -> 
+                      ?pos:int64 -> 
+                      ?addr:nativeint ->
+                       bool -> int -> memory
+  (** [memory_map_file fd shared size]: Maps [size] bytes of the file
+      [fd] into memory, and returns the memory buffer like
+      [Bigarray.Array1.map_file]. [pos] and [shared] have the same
+      meaning as there. In [addr] one can suggest a start address.
+      There is, however, no guarantee that this wish can be fulfilled.
+   *)
+ *)
+
+val memory_unmap_file : memory -> unit
+  (** Unmaps the file. The memory block must have been allocated
+      with [memory_map_file] or with [Bigarray.Array1.map_file].
+
+      {b Note that the data pointer of the bigarray is set to NULL,
+      and that any further access of the array will trigger a
+      segmentation violation!} The intention of this function is to
+      control when the file mapping is removed. Normally, this is
+      done first when the GC finalizer is run.
+
+      It is required that there are no subarrays at the time of
+      calling this function. (If so, the function does nothing.)
+   *)
+
+(** {2 Interpreting memory as values} *)
+
+val as_value : memory -> int -> 'a
+  (** [as_value mem offset]: Returns a pointer to [mem+offset]. There
+      must be a valid boxed value at this address (i.e. at 
+      the word preceding [mem+offset] there must be a valid block
+      header, followed by a valid value of the right type). However,
+      this is not checked:
+
+      {b This is an unsafe function that may crash the program if used
+      in the wrong way!}
+
+      It is possible that the memory block is deallocated while the
+      returned value still exists. Any attempt to access the value will
+      result into undefined behavior (anything from funny results
+      to crashes may happen).
+
+      Some Ocaml primitives might not work on the returned values
+      (polymorphic equality, marshalling, hashing).
+   *)
+
+exception Out_of_space
+
+val init_string : memory -> int -> int -> (int * int)
+  (** [let voffset, bytelen = init_string mem offset len]: 
+      Initializes the memory at [offset]
+      and following bytes as Ocaml string with length [len].
+      Returns in [voffset] the offset where the value starts
+      (i.e. [offset] plus one word), and in [bytelen] the number
+      of bytes used in [mem]. 
+
+      [offset] must be a multiple of the word size in bytes.
+
+      The string can be accessed with
+      {[ let s = (as_value mem voffset : string) ]}
+
+      The function is useful for initializing shared memory as string
+      so that several processes can directly access the string.
+
+      Raises [Out_of_space] if the memory block is too small.
+   *)
+
+val init_string_bytelen : int -> int
+  (** Returns [bytelen] if [init_string] was called with the passed
+      [len].
+   *)
+
+(*
+type init_value_flag =
+  | Copy_bigarray_data
+  | Copy_atoms
+
+val init_value : 
+      memory -> int -> 'a -> init_value_flag list -> (int * int)
+  (** [let voffset, bytelen = post_value mem offset v flags]:
+      Initializes the memory at [offset] and following bytes as
+      copy of the heap-allocated value [v]. 
+      Returns in [voffset] the offset where the value starts
+      (i.e. [offset] plus one word), and in [bytelen] the number
+      of bytes used in [mem]. 
+
+      The copied value can then be accessed with
+      {[ let v' = (as_value mem voffset : 'a) ]}
+
+      The input value [v] must be heap-allocated. Also, a number of
+      restrictions and caveats apply:
+      - Objects, closures, and lazy values are not supported
+      - Bigarrays are only supported if the [Copy_bigarray_data] flag
+        is given. In this case, a copy of the bigarray is also made
+        and appended to the value copy. Memory-mapped bigarrays are
+        not supported.
+      - Abstract and custom values are not supported, except
+        [int32], [int64], and [nativeint] (and bigarrays if enabled)
+      - Atoms (i.e. zero-sized blocks such as empty arrays) are only
+        supported if the [Copy_atoms] flag is present. It is, however,
+        illegal to copy atoms because they lose then their atomic
+        property. It is unclear what this may break.
+      - The input value may reside outside the Ocaml heap. This may break
+        badly written C wrappers that do not use abstract or custom
+        tags to mark foreign data.
+
+      The function raises [Out_of_space] if the memory block is too small.
+   *)
+ *)
+
+val mem_read : Unix.file_descr -> memory -> int -> int -> int
+  (** Experimental version of [Unix.read] that uses a [memory] buffer.
+     Some OS allow faster I/O when [memory] is page-aligned
+     (see [alloc_memory_pages]). Also, a copy in the stub function
+     can be avoided. Both effects can result in a considerable speedup.
+   *)
+
+
+(* Later: versions of I/O functions using memory: read, write, recv, send.
+   Use that in socket_multiplex_controller only for now (-> avoids one
+   copy!)
+
+   Maybe even sendfile (for free OS only, of course)
+ *)
