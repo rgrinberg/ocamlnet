7fde5fca63c7787d3f398e339bd22f225b0c679e
gerd <gerd@96e5b9e0-aa12-0410-9e2e-b23d24e56262>
2009-12-10 00:42:20 +0000

    exp
    git-svn-id: https://godirepo.camlcity.org/svn/lib-ocamlnet2/trunk/code@1345 96e5b9e0-aa12-0410-9e2e-b23d24e56262

--- tests/rpc/xdr_speed/read_int32_help.c
+++ tests/rpc/xdr_speed/read_int32_help.c
@@ -1,6 +1,7 @@
 /* C helpers */
 
 #include "caml/mlvalues.h"
+#include <arpa/inet.h>
 
 /* decodes the 32 bit int at s[p], converts from network byte order,
    and puts the number into the _already allocated_ nativeint n
@@ -13,7 +14,7 @@ value decode_nativeint(value sv, value pv, value nv)
 
     s = String_val(sv);
     p = Long_val(pv);
-    Nativeint_val(nv) = *((intnat *) (s+p));
+    Nativeint_val(nv) = (intnat) (ntohl (*((unsigned int *) (s+p))));
 
     return Val_unit;
 }
