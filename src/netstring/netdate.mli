f39b657c1afbd4f623f822cfa9ca00433bb90345
gerd <gerd@96e5b9e0-aa12-0410-9e2e-b23d24e56262>
2010-01-17 21:24:44 +0000

    support for RFC 3339 date/time format
    git-svn-id: https://godirepo.camlcity.org/svn/lib-ocamlnet2/trunk/code@1393 96e5b9e0-aa12-0410-9e2e-b23d24e56262

--- src/netstring/netdate.mli
+++ src/netstring/netdate.mli
@@ -25,13 +25,55 @@ val localzone : int
 val create : ?zone:int -> float -> t
   (** Convert the time (seconds since the epoch) to a date/time record *)
 
-val parse : string -> t
-  (** Parse a string and return a date/time record *)
+val parse : ?zone:int -> string -> t
+  (** Parse a string and return a date/time record.
+
+      The following particles are recognized (by example):
+      - Date: [1971/06/22]
+      - Date: [06/22/1971]
+      - Date: [1971-06-22]
+      - Date: [22-June-1971]
+      - Date: [22.06.1971]
+      - Date: [June 22, 1971]
+      - Date: [22 June 1971]
+      - Date (2 digit year): [06/22/71]
+      - Date (2 digit year): [22.06.71]
+      - Date (2 digit year): [71-06-22]
+      - Date (2 digit year): [22-June-71]
+      - Month names ([June], [Jun])
+      - Weekday names ([Monday], [Mon])
+      - Time: [14:55]
+      - Time: [14:55:28]
+      - Time: [14:55:28.6754]   (the fractional part is not returned)
+      - Time may be preceded by [T]
+      - Time zone: identifiers like [UTC], [CET], or [Z]
+      - Time zone: [+01:00], [-01:00], only following time
+      - Time zone: [+0100], [-0100], only following time
+
+      Years must have 2 or 4 digits. 2-digit years >= 70 are interpreted
+      as [1900+x]. 2-digit years < 70 are interpreted as [2000+x].
+      Support for 2-digit years will be removed in a future version
+      of Ocamlnet. (Support for 3-digit years is already removed in
+      Ocamlnet 3.0.)
+
+      Only English names of months and weekdays are recognized.
+
+      A date must be given. Time, time zones, and weekdays are optional.
+      A missing time is reported as "00:00:00". A missing weekday is
+      reported by setting [week_day=(-1)]. A missing time zone is 
+      reported by setting [zone] to the passed default.
+
+      It is not checked whether the parsed numbers make sense
+      (e.g. whether months are between 1 and 12).
+
+      Date/time strings as defined in RFC 3339 are supported since
+      Ocamlnet 3.0.
+   *)
 
 val since_epoch : t -> float
   (** Convert a date/time record into the time (seconds since the epoch) *)
 
-val parse_epoch : string -> float
+val parse_epoch : ?zone:int -> string -> float
   (** Parse a string and return the time (seconds since the epoch *)
 
 val format_to : Netchannels.out_obj_channel -> fmt:string -> t -> unit
@@ -87,6 +129,7 @@ val format_to : Netchannels.out_obj_channel -> fmt:string -> t -> unit
    *        stands for east of UTC, a minus sign for west of UTC, hours and
    *        minutes follow with two digits each and no delimiter between them
    *        (common form for RFC 822 date headers).
+   *  - [%:z]: time zone with colon, e.g. +05:00 (new since Ocamlnet 3)
    *  - [%%]: a `%' character.
    *
    *)
@@ -108,4 +151,12 @@ val mk_usenet_date : ?zone:int -> float -> string
    * Example: ["Sunday, 06-Nov-94 08:49:37 -0500"].
    *
    * Note that this format has only two digits for the year.
+   *)
+
+val mk_internet_date : ?zone:int -> float -> string
+  (** Convert the time (seconds since the epoch) to a date string that
+   * conforms to RFC 3339. This is the most modern format, and should
+   * be used if permitted by the network protocol.
+   *
+   * Example: ["1996-12-19T16:39:57.89-08:00"].
    *)
--- src/netstring/tests/test_netdate.ml
+++ src/netstring/tests/test_netdate.ml
@@ -1,6 +1,35 @@
-open Fort
 open Netdate
+open Printf
 ;;
+
+let success = ref false
+let testnr = ref 0
+
+
+let expect_pass name f =
+  printf "Test %s: %!" name;
+  success := true;
+  testnr := 1;
+  try 
+    let () = f() in
+    if !success then
+      printf "Passed\n%!"
+    else
+      printf "Failed\n%!"
+  with
+    | exn -> 
+	printf "Exception (n=%d): %s\n%!" !testnr (Printexc.to_string exn)
+
+
+let expect_equal ~printer x y =
+  if x <> y then (
+    printf "\nexpect_equal %d fails: %s <> %s\n" 
+      !testnr (printer x) (printer y);
+    success := false
+  );
+  incr testnr
+
+
 
 let print_date x =
   if x.week_day > 0 then
@@ -32,12 +61,14 @@ end;;
 expect_pass "RFC 822/1123 date parsing"
 begin fun () ->
 
+(*  - 2 digit year no longer supported
   equal_date
     { year  =  2000;  month    =  7;  day    = 13;
       hour  =    21;  minute   = 30;  second =  7;
       zone  =  5*60;  week_day =  4;
     }
     (parse "Thu, 13 Jul 00 21:30:07 +0500");
+ *)
 
   equal_date
     { year  =  2000;  month    =  7;  day    = 13;
@@ -69,6 +100,28 @@ begin fun () ->
     (parse "Thu Jul 13 21:30:07 2000");
 end;;
 
+expect_pass "RFC 3339 date parsing"
+  begin fun () ->
+    equal_date
+      { year = 1985;  month = 4; day = 12;
+	hour = 23;    minute=20; second=50;
+	zone = 0; week_day = (-1)
+      }
+      (parse "1985-04-12T23:20:50.52Z");
+     equal_date
+      { year = 1996;  month =12; day = 19;
+	hour = 16;    minute=39; second=57;
+	zone = -480; week_day = (-1)
+      }
+      (parse "1996-12-19T16:39:57-08:00");
+     equal_date
+      { year = 1937;  month =1; day = 1;
+	hour = 12;    minute=0; second=27;
+	zone = 20; week_day = (-1)
+      }
+      (parse "1937-01-01T12:00:27.87+00:20")
+ end;;
+
 expect_pass "Miscellaneous date parsing"
 begin fun () ->
 
@@ -77,21 +130,21 @@ begin fun () ->
       hour  =    10;  minute   = 35;  second = 4;
       zone  =     0;  week_day = -1;
     }
-    (parse "13 Jul 00 10:35:04 AM");
+    (parse "13 Jul 2000 10:35:04 AM");
 
   equal_date
     { year  =  2000;  month    =  7;  day    = 13;
       hour  =    22;  minute   = 35;  second = 4;
       zone  =     0;  week_day = -1;
     }
-    (parse "13 Jul 00 10:35:04 PM");
+    (parse "13 Jul 2000 10:35:04 PM");
 
   equal_date
     { year  =  2000;  month    =  7;  day    = 13;
       hour  =    22;  minute   = 35;  second = 4;
       zone  = -7*60;  week_day = -1;
     }
-    (parse "13 Jul 00 10:35:04 PM MST");
+    (parse "13 Jul 2000 10:35:04 PM MST");
 
 
   equal_float
@@ -100,6 +153,7 @@ begin fun () ->
 
 end;;
 
+(* - no longer supported 
 expect_pass "three digit years"
 begin fun () ->
   equal_date
@@ -117,6 +171,7 @@ begin fun () ->
     (parse "Thu, 13 Jul 170 21:30:07 +0500");
    
 end;;
+ *)
 
 expect_pass "date formatting"
 begin fun () ->
@@ -154,6 +209,7 @@ begin fun () ->
   equal_str "2001"                       (format "%Y" t_date);
   equal_str "01"                         (format "%y" t_date);
   equal_str "+0000"                      (format "%z" t_date);
+  equal_str "+00:00"                     (format "%:z" t_date);
   equal_str "%"                          (format "%%" t_date);
 
 end;;
@@ -167,6 +223,9 @@ begin fun () ->
   equal_str
     "Sunday, 09-Sep-01 01:46:40 +0000"
     (mk_usenet_date t);
+  equal_str
+    "2001-09-09T01:46:40+00:00"
+    (mk_internet_date t);
 
 end;;
 
