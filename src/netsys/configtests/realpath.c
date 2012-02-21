31b6ded86548e56e8eed7d8c65abaafa4a3cce14
gerd <gerd@96e5b9e0-aa12-0410-9e2e-b23d24e56262>
2012-02-21 16:09:55 +0000

    experiment
    git-svn-id: https://godirepo.camlcity.org/svn/lib-ocamlnet2/trunk/code@1719 96e5b9e0-aa12-0410-9e2e-b23d24e56262

--- src/netsys/configtests/realpath.c
+++ src/netsys/configtests/realpath.c
@@ -9,6 +9,11 @@
 
 /* reminder: we return the exit code, and 0 means success */
 
+/* We only accept here the variant specified by POSIX.1-2008 where
+   the second arg can be NULL. Older versions are rejected (usually
+   by sigsegv)
+*/
+
 value check(value dummy) {
     realpath("/", NULL);
     return Val_int(0);
