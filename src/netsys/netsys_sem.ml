3318ad827b4221531873ab374acc22c482f81b00
gerd <gerd@96e5b9e0-aa12-0410-9e2e-b23d24e56262>
2012-04-18 16:03:18 +0000

    enhanced impl: avoid that too many semaphores are open at a time
    git-svn-id: https://godirepo.camlcity.org/svn/lib-ocamlnet2/trunk/code@1776 96e5b9e0-aa12-0410-9e2e-b23d24e56262

--- src/netsys/netsys_sem.ml
+++ src/netsys/netsys_sem.ml
@@ -31,6 +31,12 @@ module Emu = struct
        the code.
      *)
 
+  let n_active = Netsys_posix.sysconf_open_max() / 4
+    (* max number of open semaphores (normally, each open sem consumes
+       a file descr). It is tried not to exceed this value, but in some
+       situations this will nevertheless occur.
+     *)
+
   (* Note that there is a basic problem with [mutex]: If the process dies
      while the mutex is in locked state, no other process will ever
      again get the lock. TODO: We could protect against this by recording
@@ -38,23 +44,137 @@ module Emu = struct
    *)
 
   type anon_semaphore =
-      { sem : Netsys_posix.named_semaphore;
-	num : int
+      { mutable sem : Netsys_posix.named_semaphore option;
+        mutable use_count : int;
+	num : int;                              (* number in the container *)
+        mutable freshness : int;                (* freshness for lru cache *)
+        cont_id : int;                   (* Container ID for cross-lookups *)
       }
+      (* The active version of a semaphore. There is only one instance 
+         of this record per process and semaphore. [sem] is set to the
+         backing named semaphore. [use_counts] counts the number of active uses
+         this process (values > 1 are possible in multithreaded programs),
+         such as ongoing semaphore operations. When [use_count > 0], the
+         semaphore cannot be closed.
 
+         [num] is the number of this semaphore in the container. This means,
+         [cont.used.{num} = true].
+
+         The value [freshness] is set to a new maximum whenever the semaphore
+         is used.
+       *)
+         
   type container =
       { prefix : string;
+        id : int;
 	used : (char,Bigarray.int8_unsigned_elt,Bigarray.c_layout) Bigarray.Array1.t;
 	active : (int, anon_semaphore) Hashtbl.t;
-	mutex : Netsys_posix.named_semaphore;
+	mutex : Netsys_posix.named_semaphore;  (* protects only [used] *)
+        mutable open_sems : int;
+        mutable maxfresh : int;
       }
+      (* The container. There is only one instance of this record per process
+         and [prefix]. This instance also gets a unique [id].
+
+         The possible semaphores in the container have numbers 0 to n-1,
+         where n is the size of the [used] array. When a number is allocated,
+         the [used] array is set to [true] at the corresponding index.
+
+         If a semaphore is opened, it is put into [active]. If a semaphore
+         is closed, it is not removed from [active], however. This first
+         happens when it is destroyed. The counter [open_sems] contains the
+         number of open semaphores in [active] (with [sem<>None]).
+       *)
+
+  (* Forks: the construction needs to be compatible with fork(), at least for
+     single-threaded apps. Both memory mappings ([used]) and named semaphores
+     survive forks, so there should not be any problem.
+
+     In mt apps: there are two problems:
+     - [use_count] would have to be reset to 0 after a fork
+     - [proc_mutex] would have to be reinitialized
+
+     We do not attempt here to solve these problems.
+   *)
+     
+
+
+  let proc_mutex = !Netsys_oothr.provider # create_mutex()
+
+  let cont_by_id = Hashtbl.create 7
+  let cont_by_prefix = Hashtbl.create 7
+
+  let sem_name cont k =
+    cont.prefix ^ "_sem" ^ string_of_int k
+
+
+  let lookup_cont_by_id =
+    Netsys_oothr.serialize 
+      proc_mutex
+      (fun id ->
+         Hashtbl.find cont_by_id id
+      )
+
+  let lookup_cont_by_prefix =
+    Netsys_oothr.serialize 
+      proc_mutex
+      (fun p ->
+         Hashtbl.find cont_by_prefix p
+      )
+
+  let add_cont =
+    Netsys_oothr.serialize 
+      proc_mutex
+      (fun c ->
+         Hashtbl.replace cont_by_id c.id c;
+         Hashtbl.replace cont_by_prefix c.prefix c
+      )
+
+  let del_cont =
+    Netsys_oothr.serialize 
+      proc_mutex
+      (fun c ->
+         Hashtbl.remove cont_by_id c.id;
+         Hashtbl.remove cont_by_prefix c.prefix
+      )
+
+  let lookup_or_add_cont f =
+    Netsys_oothr.serialize 
+      proc_mutex
+      (fun p ->
+         try
+           Hashtbl.find cont_by_prefix p
+         with
+           | Not_found ->
+               let c = f p in
+               Hashtbl.replace cont_by_id c.id c;
+               Hashtbl.replace cont_by_prefix c.prefix c;
+               c
+      )
+
+  let freshest =
+    Netsys_oothr.serialize 
+      proc_mutex
+      (fun c ->
+         let fn = c.maxfresh+1 in
+         c.maxfresh <- fn;
+         fn
+      )
+
+  let mod_use_count d =
+    Netsys_oothr.serialize 
+      proc_mutex
+      (fun usem ->
+         usem.use_count <- usem.use_count + d
+      )
+
 
   let have_anon_semaphores() =
     Netsys_posix.have_named_posix_semaphores()
 
   let prefix cont = cont.prefix
 
-  let container prefix =
+  let load_container prefix =
     let fd = 
       Netsys_posix.shm_open
 	(prefix ^ "_sems")
@@ -81,12 +201,20 @@ module Emu = struct
 	{ prefix = prefix;
 	  used = used;
 	  active = Hashtbl.create 47;
-	  mutex = mutex
+	  mutex = mutex;
+          open_sems = 0;
+          maxfresh = 0;
+          id = Oo.id (object end);
 	}
       with error ->
 	Unix.close fd;
 	raise error
     )
+
+  let container prefix =
+    lookup_or_add_cont
+      load_container
+      prefix
 
   let lock cont =
     dlogr (fun () -> sprintf "lock waiting prefix=%s" cont.prefix);
@@ -96,9 +224,6 @@ module Emu = struct
   let unlock cont =
     dlogr (fun () -> sprintf "unlock prefix=%s" cont.prefix);
     Netsys_posix.sem_post cont.mutex
-
-  let sem_name cont k =
-    cont.prefix ^ "_sem" ^ string_of_int k
 
   let unlink prefix =
     (* A radical way to get rid of all persistent objects, without acquiring
@@ -118,38 +243,130 @@ module Emu = struct
     unlink prefix;
     container prefix
 
+  let drop_active_lk cont =
+    (* While locked: find the sem with the lowest freshness and close it.
+       Consider only sems without users.
+     *)
+    let old_open_sems = cont.open_sems in
+    ( try
+        while cont.open_sems+1 > n_active do
+          let usem_opt =
+            Hashtbl.fold
+              (fun _ usem best_usem_opt ->
+                 if usem.use_count = 0 && usem.sem <> None then (
+                   match best_usem_opt with
+                     | None -> (Some usem)
+                     | Some b ->
+                         if usem.freshness < b.freshness then
+                           Some usem
+                         else
+                           best_usem_opt
+                 )
+                 else
+                   best_usem_opt
+              )
+              cont.active
+              None in
+          match usem_opt with
+            | None -> raise Not_found (* leave loop *)
+            | Some usem ->
+                ( match usem.sem with
+                    | None -> assert false
+                    | Some sem ->
+                        dlogr
+                          (fun () ->
+                             sprintf "drop_active: closing prefix=%s k=%d"
+                               cont.prefix usem.num);
+                        Netsys_posix.sem_close sem;
+                        usem.sem <- None;
+                        cont.open_sems <- cont.open_sems - 1;
+                )
+        done
+      with
+        | Not_found -> ()
+    );
+    dlogr
+      (fun () ->
+         sprintf "drop_active: old_open=%d now_open=%d"
+           old_open_sems cont.open_sems
+      )
+
+  let drop_active cont =
+    Netsys_oothr.serialize 
+      proc_mutex
+      (fun () -> drop_active_lk cont)
+      ()
+
+  let use_sem cont =
+    Netsys_oothr.serialize 
+      proc_mutex
+      (fun usem ->
+         match usem.sem with
+           | None ->
+               dlogr
+                 (fun () ->
+                    sprintf "Reloading sem prefix=%s k=%d"
+                      cont.prefix usem.num
+                 );
+               if cont.open_sems >= n_active then
+                 drop_active cont;
+	       let sem =
+	         Netsys_posix.sem_open
+	           (sem_name cont usem.num)
+	           []
+	           0
+	           0 in
+               usem.sem <- Some sem;
+               cont.open_sems <- cont.open_sems + 1;
+               usem.use_count <- usem.use_count + 1;
+               sem
+           | Some sem ->
+               usem.use_count <- usem.use_count + 1;
+               sem
+      )    
+      
+
   let lookup cont k =
     (* Look the existing semaphore #k up. ENOENT if not found *)
-    lock cont;
-    try
-      try 
-        let usem = Hashtbl.find cont.active k in
-        dlogr (fun () -> 
-                 sprintf "lookup prefix=%s k=%d active" cont.prefix k);
-        unlock cont;
-        usem
-      with Not_found ->
-	let sem =
-	  Netsys_posix.sem_open
-	    (sem_name cont k)
-	    []
-	    0
-	    0 in
-	let usem =
-	  { sem = sem;
-	    num = k
-	  } in
-        dlogr (fun () -> 
-                 sprintf "lookup prefix=%s k=%d opened" cont.prefix k);
-	Hashtbl.add cont.active k usem;
-	unlock cont;
-	usem
-    with error ->
-      dlogr (fun () -> 
-               sprintf "lookup prefix=%s k=%d error=%s"
-                 cont.prefix k (Netexn.to_string error));
-      unlock cont;
-      raise error
+    let fn = freshest cont in
+    Netsys_oothr.serialize 
+      proc_mutex
+      (fun () ->
+         try
+           try 
+             let usem = Hashtbl.find cont.active k in
+             dlogr (fun () -> 
+                      sprintf "lookup prefix=%s k=%d active" cont.prefix k);
+             (* Might still be closed! *)
+             usem
+           with Not_found ->
+             if cont.open_sems >= n_active then
+               drop_active_lk cont;
+	     let sem =
+	       Netsys_posix.sem_open
+	         (sem_name cont k)
+	         []
+	         0
+	         0 in
+	     let usem =
+	       { sem = Some sem;
+	         num = k;
+                 freshness = fn;
+                 cont_id = cont.id;
+                 use_count = 0;
+	       } in
+             dlogr (fun () -> 
+                      sprintf "lookup prefix=%s k=%d opened" cont.prefix k);
+	     Hashtbl.add cont.active k usem;
+             cont.open_sems <- cont.open_sems + 1;
+	     usem
+         with error ->
+           dlogr (fun () -> 
+                    sprintf "lookup prefix=%s k=%d error=%s"
+                      cont.prefix k (Netexn.to_string error));
+           raise error
+      )
+      ()
 
 
   let find_unused cont =
@@ -186,77 +403,166 @@ module Emu = struct
 
   let sem_init cont mem pos pshared init_value =
     let k = find_unused cont in
-    let sem =
-      Netsys_posix.sem_open
-	(sem_name cont k)
-	[ Netsys_posix.SEM_O_CREAT; Netsys_posix.SEM_O_EXCL ]
-	0o600
-	init_value in
-    let usem =
-      { sem = sem;
-	num = k
-      } in
-    lock cont;
-    Hashtbl.replace cont.active k usem;
-    unlock cont;
-    mem.{ pos } <- Char.chr (k land 0xff);
-    mem.{ pos+1 } <- Char.chr (k lsr 8);
-    dlogr (fun () -> 
-             sprintf "sem_init prefix=%s k=%d" cont.prefix k);
-    usem
+    let fn = freshest cont in
+    Netsys_oothr.serialize 
+      proc_mutex
+      (fun () ->
+         (* First check whether k is still in [active]. This can happen when
+            another process destroyed the semaphore, but this process did not
+            see it yet. We don't destroy it here again (too error-prone).
+          *)
+         ( try
+             let usem = Hashtbl.find cont.active k in
+             if usem.sem <> None then
+               cont.open_sems <- cont.open_sems - 1;
+             Hashtbl.remove cont.active k
+           with Not_found -> ()
+         );
+         try
+           if cont.open_sems >= n_active then
+             drop_active_lk cont;
+           let sem =
+             Netsys_posix.sem_open
+	       (sem_name cont k)
+	       [ Netsys_posix.SEM_O_CREAT; Netsys_posix.SEM_O_EXCL ]
+	       0o600
+	       init_value in
+           let usem =
+             { sem = Some sem;
+	       num = k;
+               freshness = fn;
+               cont_id = cont.id;
+               use_count = 0;
+             } in
+           Hashtbl.replace cont.active k usem;
+           cont.open_sems <- cont.open_sems + 1;
+           mem.{ pos } <- Char.chr (k land 0xff);
+           mem.{ pos+1 } <- Char.chr (k lsr 8);
+           dlogr (fun () -> 
+                    sprintf "sem_init prefix=%s k=%d" cont.prefix k);
+           usem
+         with
+           | error ->
+               dlogr (fun () -> 
+                        sprintf "sem_init prefix=%s k=%d error=%s"
+                          cont.prefix k (Netexn.to_string error));
+               raise error
+      )
+      ()
 
   let sem_idestroy cont k =
-    Hashtbl.remove cont.active k;
+    ( try
+        let usem = Hashtbl.find cont.active k in
+        if usem.sem <> None then
+          cont.open_sems <- cont.open_sems - 1;
+        Hashtbl.remove cont.active k;
+      with Not_found -> ()
+    );
     ( try
 	Netsys_posix.sem_unlink
 	  (sem_name cont k)
       with _ -> ()
-    );
-    mark_unused cont k
+    )
 
-  let sem_destroy cont usem =
-    dlogr (fun () -> 
-             sprintf "sem_destroy prefix=%s k=%d" cont.prefix usem.num);
-    Netsys_posix.sem_close usem.sem;
+  let sem_release cont k =
     lock cont;
-    sem_idestroy cont usem.num;
+    mark_unused cont k;
     unlock cont
+
+  let sem_destroy cont =
+    Netsys_oothr.serialize 
+      proc_mutex
+      (fun usem ->
+         let w = usem.use_count in
+         dlogr (fun () -> 
+                  sprintf "sem_destroy prefix=%s k=%d use_count=%d" 
+                    cont.prefix usem.num w);
+         if w > 0 then
+           failwith "Netsys_sem.sem_destroy: there are still threads in sem_wait";
+         ( match usem.sem with
+             | None -> ()
+             | Some sem ->
+                 Netsys_posix.sem_close sem;
+                 usem.sem <- None
+         );
+         sem_idestroy cont usem.num;
+         sem_release cont usem.num;
+      )
 
   let drop_container cont =
     dlogr (fun () -> 
              sprintf "drop_container start prefix=%s" cont.prefix);
-    lock cont;
-    for k = 0 to n-1 do
-      if cont.used.{k} = '\001' then
-	sem_idestroy cont k
-    done;
-    ( try
-	Netsys_posix.shm_unlink (cont.prefix ^ "_sems")
-      with _ -> ()
-    );
-    ( try
-	Netsys_posix.sem_unlink (cont.prefix ^ "_contsem")
-      with _ -> ()
-    );
-    dlogr (fun () -> 
-             sprintf "drop_container finish prefix=%s" cont.prefix);
-    unlock cont
+    Netsys_oothr.serialize 
+      proc_mutex
+      (fun () ->
+         lock cont;
+         for k = 0 to n-1 do
+           if cont.used.{k} = '\001' then
+	     sem_idestroy cont k
+         done;
+         ( try
+	     Netsys_posix.shm_unlink (cont.prefix ^ "_sems")
+           with _ -> ()
+         );
+         ( try
+	     Netsys_posix.sem_unlink (cont.prefix ^ "_contsem")
+           with _ -> ()
+         );
+         del_cont cont;  (* remove from hash tables *)
+         dlogr (fun () -> 
+                  sprintf "drop_container finish prefix=%s" cont.prefix);
+         unlock cont
+      )
+      ()
+
+
+  let get_sem usem =
+    let cont = 
+      try
+        lookup_cont_by_id usem.cont_id
+      with
+        | Not_found -> assert false in
+    let fn = freshest cont in
+    usem.freshness <- fn;
+    use_sem cont usem   (* also increases use_count by 1 *)
 
   let sem_getvalue usem =
-    Netsys_posix.sem_getvalue usem.sem
+    let sem = get_sem usem in
+    try
+      let v = Netsys_posix.sem_getvalue sem in
+      mod_use_count (-1) usem;
+      v
+    with error ->
+      mod_use_count (-1) usem;
+      raise error
 
   let sem_post usem =
     dlogr (fun () -> 
              sprintf "sem_post k=%d" usem.num);
-    Netsys_posix.sem_post usem.sem
+    let sem = get_sem usem in
+    try
+      Netsys_posix.sem_post sem;
+      mod_use_count (-1) usem;
+    with error ->
+      mod_use_count (-1) usem;
+      raise error
 
   let sem_wait usem wb =
     dlogr (fun () -> 
              sprintf "sem_wait waiting k=%d" usem.num);
-    Netsys_posix.sem_wait usem.sem wb;
-    dlogr (fun () -> 
-             sprintf "sem_wait acquired k=%d" usem.num)
-
+    let sem = get_sem usem in
+    try
+      Netsys_posix.sem_wait sem wb;
+      mod_use_count (-1) usem;
+      dlogr (fun () -> 
+               sprintf "sem_wait acquired k=%d" usem.num)
+    with
+      | error ->
+          mod_use_count (-1) usem;
+          dlogr (fun () -> 
+                   sprintf "sem_wait exn=%s k=%d"
+                     (Netexn.to_string error) usem.num);
+          raise error
 end
 
 
